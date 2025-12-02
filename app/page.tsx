5eb8ab9c21273e79c18919cab473e291bb146791
MrSeager <seager1993@gmail.com>
2025-12-02 12:03:04 +0200

    12/2/2025
    navbar is ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  // #027373 #038C7F #A9D9D0 #F2E7DC #0D0D0D
+
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="bg-[#5B578E] flex min-h-screen w-full max-w-[100rem] flex-col items-center justify-between py-32 px-16">
+    <div className="bg-[#F2E7DC] flex items-center justify-center font-sans">
+      <main className="bg-[#F2E7DC] h-[1500px] shadow-lg flex w-full max-w-[100rem] flex-col items-center justify-between py-32 px-16">
 
       </main>
     </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,42 +5,117 @@ import Link from "next/link"
 //Icons
 import { IoMenu, IoClose } from "react-icons/io5";
 import { CgWebsite } from "react-icons/cg";
+import { FaLinkedinIn } from "react-icons/fa";
+import { SiRefinedgithub } from "react-icons/si";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return (
-        <nav className=''>
-            <div className="bg-[#191827] max-w-[100rem] mx-auto px-4 sm:px-6 lg:px-4">
+        <nav className='bg-[#F2E7DC] relative border-black sticky w-full top-0 z-50'>
+            <div className="relative shadow-lg bg-[#0D0D0D] text-[#F2E7DC] max-w-[100rem] mx-auto px-4 sm:px-6 lg:px-4">
                 <div className="flex justify-between items-center py-5">
                     <div className='flex items-center gap-3'>
-                        <CgWebsite size={50} />
+                        <CgWebsite size={50} className='xl:block sm:hidden lg:block' />
                         <h1 className='font-bold text-[25px]'>Mr. Seager<br />
                         Portfolio</h1>
                     </div>
                     <div className='md:flex gap-5 hidden'>
-                        <div className="font-semibold flex flex-col items-start">
-                            <button type="button" className="uppercase">html</button>
-                            <button type="button" className="">react</button>
-                            <button type="button" className="">next.js</button>
-                        </div>
-                        <div className="font-semibold flex flex-col">
-                            <button type="button" className="">css</button>
-                            <button type="button" className="">bootstrap</button>
-                            <button type="button" className="">tailwind</button>
-                            <button type="button" className="">reactspring</button>
-                        </div>
-                        <div className="font-semibold flex flex-col items-end">
-                            <button type="button" className="">javascript</button>
-                            <button type="button" className="">typescript</button>
+                        <div className="font-semibold flex items-center justify-center gap-1 flex-wrap max-w-[550px]">
+                            <button 
+                                type="button" 
+                                className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                          hover:bg-transparent hover:text-[#F2E7DC]
+                                          focus:bg-transparent focus:text-[#F2E7DC]"
+                            >
+                                html
+                            </button>
+                            <button 
+                                type="button"
+                                className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                          hover:bg-transparent hover:text-[#F2E7DC]
+                                          focus:bg-transparent focus:text-[#F2E7DC]"
+                            >
+                                react
+                            </button>
+                            <button 
+                                type="button"
+                                className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                          hover:bg-transparent hover:text-[#F2E7DC]
+                                          focus:bg-transparent focus:text-[#F2E7DC]"
+                            >
+                                next.js
+                            </button>
+                            <button 
+                                type="button"
+                                className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                          hover:bg-transparent hover:text-[#F2E7DC]
+                                          focus:bg-transparent focus:text-[#F2E7DC]"
+                            >
+                                css
+                            </button>
+                            <button 
+                                type="button" 
+                                className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                          hover:bg-transparent hover:text-[#F2E7DC]
+                                          focus:bg-transparent focus:text-[#F2E7DC]"
+                            >
+                                bootstrap
+                            </button>
+                            <button 
+                                type="button"
+                                className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                          hover:bg-transparent hover:text-[#F2E7DC]
+                                          focus:bg-transparent focus:text-[#F2E7DC]"
+                            >
+                                tailwind
+                            </button>
+                            <button 
+                                type="button"
+                                className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                          hover:bg-transparent hover:text-[#F2E7DC]
+                                          focus:bg-transparent focus:text-[#F2E7DC]"
+                            >
+                                reactspring
+                            </button>
+                            <button 
+                                type="button"
+                                className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                          hover:bg-transparent hover:text-[#F2E7DC]
+                                          focus:bg-transparent focus:text-[#F2E7DC]"
+                            >
+                                javascript
+                            </button>
+                            <button 
+                                type="button"
+                                className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                          hover:bg-transparent hover:text-[#F2E7DC]
+                                          focus:bg-transparent focus:text-[#F2E7DC]"
+                            >
+                                typescript
+                            </button>
                         </div>
                     </div>
 
-                    <div className="hidden md:flex space-x-4">
-                        <Link href="/" className="">1</Link>
-                        <Link href="/" className="">2</Link>
-                        <Link href="/" className="">3</Link>
-                        <Link href="/" className="">4</Link>
+                    <div className="hidden md:flex flex-col gap-2">
+                        <Link 
+                            href="https://www.linkedin.com/in/sergiy-b-623426159/"
+                            target='_blank'
+                            className="cursor-pointer bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] p-2 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            <FaLinkedinIn />
+                        </Link>
+                        <Link 
+                            href="https://github.com/MrSeager"
+                            target='_blank'
+                            className="cursor-pointer bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] p-2 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            <SiRefinedgithub />
+                        </Link>
                     </div>
 
                     <div className="md:hidden">
@@ -51,15 +126,106 @@ export default function Navbar() {
                 </div>
             </div>
 
-            <div 
-                className={`transition-all duration-300 ease-in-out ${
-                            isOpen ? "max-h-100 opacity-100" : "max-h-0 opacity-0"} 
-                            uppercase font-semibold md:hidden px-4 pb-4 flex flex-col items-center gap-2`}
+            <div className={`overflow-hidden transition-all px-3 absolute left-0 right-0 w-full bg-[#0D0D0D] duration-500 ease-in-out flex flex-col items-center gap-3
+                ${isOpen ? 'max-h-screen py-3' : 'max-h-0 pointer-events-none'}`}
             >
-                <Link href="/" className="block focus:text-shadow-md focus:scale-105 transition-transform duration-500">Discover</Link>
-                <Link href="/" className="block focus:text-shadow-md focus:scale-105 transition-transform duration-500">Search</Link>
-                <Link href="/" className="block focus:text-shadow-md focus:scale-105 transition-transform duration-500">Why</Link>
-                <Link href="/" className="block focus:text-shadow-md focus:scale-105 transition-transform duration-500">Cart</Link>
+                <div className='flex flex-col gap-5'>
+                    <div className="font-semibold flex items-center justify-center gap-1 flex-wrap max-w-[550px]">
+                        <button 
+                            type="button" 
+                            className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            html
+                        </button>
+                        <button 
+                            type="button"
+                            className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            react
+                        </button>
+                        <button 
+                            type="button"
+                            className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            next.js
+                        </button>
+                        <button 
+                            type="button"
+                            className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            css
+                        </button>
+                        <button 
+                            type="button" 
+                            className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            bootstrap
+                        </button>
+                        <button 
+                            type="button"
+                            className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            tailwind
+                        </button>
+                        <button 
+                            type="button"
+                            className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            reactspring
+                        </button>
+                        <button 
+                            type="button"
+                            className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            javascript
+                        </button>
+                        <button 
+                            type="button"
+                            className="text-sm cursor-pointer uppercase bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] px-3 py-1 rounded rounded-4 duration-500
+                                        hover:bg-transparent hover:text-[#F2E7DC]
+                                        focus:bg-transparent focus:text-[#F2E7DC]"
+                        >
+                            typescript
+                        </button>
+                    </div>
+                </div>
+
+                <div className="flex gap-2 justify-between w-full">
+                    <Link 
+                        href="https://www.linkedin.com/in/sergiy-b-623426159/"
+                        target='_blank'
+                        className="cursor-pointer bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] p-2 rounded rounded-4 duration-500
+                                    hover:bg-transparent hover:text-[#F2E7DC]
+                                    focus:bg-transparent focus:text-[#F2E7DC]"
+                    >
+                        <FaLinkedinIn />
+                    </Link>
+                    <Link 
+                        href="https://github.com/MrSeager"
+                        target='_blank'
+                        className="cursor-pointer bg-[#F2E7DC] border-2 border-[#F2E7DC] text-[#0D0D0D] p-2 rounded rounded-4 duration-500
+                                    hover:bg-transparent hover:text-[#F2E7DC]
+                                    focus:bg-transparent focus:text-[#F2E7DC]"
+                    >
+                        <SiRefinedgithub />
+                    </Link>
+                </div>
             </div>
         </nav>
     );
