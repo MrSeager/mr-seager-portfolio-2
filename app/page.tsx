2b22397e7322b584a4b8d11307344034a86a0db1
MrSeager <seager1993@gmail.com>
2025-12-10 12:09:06 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import HomeClient from "@/components/HomeClient";
 import type { PortfolioDataProps } from "@/types/portfolioTypes";
 
+import portfolioData from "@/public/data/portfolio.json";
+
 export default async function Home() {
   // #FFFFFF #B2B2B2 #000983 #F20505 #5E5E5E #000000
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
-
-  const res = await fetch(new URL("/data/portfolio.json", baseUrl).toString(), {
-    next: { revalidate: 3600 },
-  });
-  const data: PortfolioDataProps = await res.json();
+  const data: PortfolioDataProps = portfolioData;
 
   return (
     <div className="select-none bg-[#5E5E5E] flex flex-col items-center justify-center font-sans">
